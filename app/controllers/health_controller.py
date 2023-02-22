aa2a3efd32738289f8929b75d0d019618ccee1c0
yazgiraldoa <edygiraldo.97@gmail.com>
2023-02-21 22:54:47 -0500

    [feature][SHOR-4] | Adding health endpoint

---
+++ app/controllers/health_controller.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from app.Responses.status_response import StatusResponse
+from app.configurations.settings import settings
+
+api = APIRouter()
+
+
+@api.get('/health')
+def health_check() -> StatusResponse:
+    return StatusResponse(
+        status="up",
+        version=settings.commit,
+        description=settings.description
+    )
